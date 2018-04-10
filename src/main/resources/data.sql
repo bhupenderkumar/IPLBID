dfbb8782aa85920f18f83945541ee98fbb20b635
bhukumar <bhukumar@WW7INNOI01U0428.asi.bvcorp.corp>
2018-04-10 09:40:44 +0530

    comminting data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,70 @@
-INSERT INTO `role` VALUES (1,'ADMIN');
+
+
+INSERT into role values(1,'ADMIN');
+
+/*insert into matches  values (1,10,1,2 ,1, 'MI vs CSK','2010-01-01');*/
+/*insert into matches  values (2,10,1,2 ,1, 'KXIP vs DD','2010-01-01');*/
+
+
+
+  insert into ipl_matches  values ('April 7','Mumbai Indians','Chennai Super Kings','Mumbai','8:00 PM');
+  insert into ipl_matches  values ('April 8','Delhi Daredevils','Kings XI Punjab','Delhi','4:00 PM');
+  insert into ipl_matches  values ('April 8','Kolkata Knight Riders','Royal Challengers Bangalore','Kolkata','8:00 PM');
+  insert into ipl_matches  values ('April 9','Sunrisers Hyderabad','Rajasthan Royals','Hyderabad','8:00 PM');
+  insert into ipl_matches  values ('April 10','Chennai Super Kings','Kolkata Knight Riders','Chennai','8:00 PM');
+  insert into ipl_matches  values ('April 11','Rajasthan Royals','Delhi Daredevils','Jaipur','8:00 PM');
+  insert into ipl_matches  values ('April 12','Sunrisers Hyderabad','Mumbai Indians','Hyderabad','8:00 PM');
+  insert into ipl_matches  values ('April 13','Royal Challengers Bangalore','Kings XI Punjab','Bangalore','8:00 PM');
+  insert into ipl_matches  values ('April 14','Mumbai Indians','Delhi Daredevils','Mumbai','4:00 PM');
+  insert into ipl_matches  values ('April 14','Kolkata Knight Riders','Sunrisers Hyderabad','Kolkata','8:00 PM');
+  insert into ipl_matches  values ('April 15','Royal Challengers Bangalore','Rajasthan Royals','Bangalore','4:00 PM');
+  insert into ipl_matches  values ('April 15','Kings XI Punjab','Chennai Super Kings','Indore','8:00 PM');
+  insert into ipl_matches  values ('April 16','Kolkata Knight Riders','Delhi Daredevils','Kolkata','8:00 PM');
+  insert into ipl_matches  values ('April 17','Mumbai Indians','Royal Challengers Bangalore','Mumbai','8:00 PM');
+  insert into ipl_matches  values ('April 18','Rajasthan Royals','Kolkata Knight Riders','Jaipur','8:00 PM');
+  insert into ipl_matches  values ('April 19','Kings XI Punjab','Sunrisers Hyderabad','Indore','8:00 PM');
+  insert into ipl_matches  values ('April 20','Chennai Super Kings','Rajasthan Royals','Chennai','8:00 PM');
+  insert into ipl_matches  values ('April 21','Kolkata Knight Riders','Kings XI Punjab','Kolkata','4:00 PM');
+  insert into ipl_matches  values ('April 21','Delhi Daredevils','Royal Challengers Bangalore','Delhi','8:00 PM');
+  insert into ipl_matches  values ('April 22','Sunrisers Hyderabad','Chennai Super Kings','Hyderabad','4:00 PM');
+  insert into ipl_matches  values ('April 22','Rajasthan Royals','Mumbai Indians','Jaipur','8:00 PM');
+  insert into ipl_matches  values ('April 22','Rajasthan Royals','Mumbai Indians','Jaipur','8:00 PM');
+  insert into ipl_matches  values ('April 23','Kings XI Punjab','Delhi Daredevils','Indore','8:00 PM');
+  insert into ipl_matches  values ('April 24','Mumbai Indians','Sunrisers Hyderabad','Mumbai','8:00 PM');
+  insert into ipl_matches  values ('April 25','Royal Challengers Bangalore','Chennai Super Kings','Bangalore','8:00 PM');
+  insert into ipl_matches  values ('April 26','Sunrisers Hyderabad','Kings XI Punjab','Hyderabad','8:00 PM');
+  insert into ipl_matches  values ('April 27','Delhi Daredevils','Kolkata Knight Riders','Delhi','8:00 PM');
+  insert into ipl_matches  values ('April 28','Chennai Super Kings','Mumbai Indians','Chennai','8:00 PM');
+  insert into ipl_matches  values ('April 29','Rajasthan Royals','Sunrisers Hyderabad','Jaipur','4:00 PM');
+  insert into ipl_matches  values ('April 29','Royal Challengers Bangalore','Kolkata Knight Riders','Bangalore','8:00 PM');
+  insert into ipl_matches  values ('April 30','Chennai Super Kings','Delhi Daredevils','Chennai','8:00 PM');
+  insert into ipl_matches  values ('May 1','Royal Challengers Bangalore','Mumbai Indians','Bangalore','8:00 PM');
+  insert into ipl_matches  values ('May 2','Delhi Daredevils','Rajasthan Royals','Delhi','8:00 PM');
+  insert into ipl_matches  values ('May 3','Kolkata Knight Riders','Chennai Super Kings','Kolkata','8:00 PM');
+  insert into ipl_matches  values ('May 4','Kings XI Punjab','Mumbai Indians','Mohali','8:00 PM');
+  insert into ipl_matches  values ('May 5','Chennai Super Kings','Royal Challengers Bangalore','Chennai','4:00 PM');
+  insert into ipl_matches  values ('May 5','Sunrisers Hyderabad','Delhi Daredevils','Hyderabad','8:00 PM');
+  insert into ipl_matches  values ('May 6','Mumbai Indians','Kolkata Knight Riders','Mumbai','4:00 PM');
+  insert into ipl_matches  values ('May 6','Kings XI Punjab','Rajasthan Royals','Mohali','8:00 PM');
+  insert into ipl_matches  values ('May 7','Sunrisers Hyderabad','Royal Challengers Bangalore','Hyderabad','8:00 PM');
+  insert into ipl_matches  values ('May 8','Rajasthan Royals','Kings XI Punjab','Jaipur','8:00 PM');
+  insert into ipl_matches  values ('May 9','Kolkata Knight Riders','Mumbai Indians','Kolkata','8:00 PM');
+  insert into ipl_matches  values ('May 10','Delhi Daredevils','Sunrisers Hyderabad','Delhi','8:00 PM');
+  insert into ipl_matches  values ('May 11','Rajasthan Royals','Chennai Super Kings','Jaipur','8:00 PM');
+  insert into ipl_matches  values ('May 12','Kings XI Punjab','Kolkata Knight Riders','Mohali','4:00 PM');
+  insert into ipl_matches  values ('May 12','Royal Challengers Bangalore','Delhi Daredevils','Bangalore','8:00 PM');
+  insert into ipl_matches  values ('May 13','Chennai Super Kings','Sunrisers Hyderabad','Chennai','4:00 PM');
+  insert into ipl_matches  values ('May 13','Mumbai Indians','Rajasthan Royals','Mumbai','8:00 PM');
+  insert into ipl_matches  values ('May 14','Kings XI Punjab','Royal Challengers Bangalore','Mohali','8:00 PM');
+  insert into ipl_matches  values ('May 15','Kolkata Knight Riders','Rajasthan Royals','Kolkata','8:00 PM');
+  insert into ipl_matches  values ('May 16','Mumbai Indians','Kings XI Punjab','Mumbai','8:00 PM');
+  insert into ipl_matches  values ('May 17','Royal Challengers Bangalore','Sunrisers Hyderabad','Bangalore','8:00 PM');
+  insert into ipl_matches  values ('May 18','Delhi Daredevils','Chennai Super Kings','Delhi','8:00 PM');
+  insert into ipl_matches  values ('May 19','Rajasthan Royals','Royal Challengers Bangalore','Jaipur','4:00 PM');
+  insert into ipl_matches  values ('May 19','Sunrisers Hyderabad','Kolkata Knight Riders','Hyderabad','8:00 PM');
+  insert into ipl_matches  values ('May 20','Delhi Daredevils','Mumbai Indians','Delhi','4:00 PM');
+  insert into ipl_matches  values ('May 20','Chennai Super Kings','Kings XI Punjab','Chennai','8:00 PM');
+  insert into ipl_matches  values ('May 22','Qualifier 1','Qualifier 1','Mumbai','8:00 PM');
+  insert into ipl_matches  values ('May 23','Eliminator','Eliminator','TBC','8:00 PM');
+  insert into ipl_matches  values ('May 25','Qualifier 2','Qualifier 2','TBC','8:00 PM');
+  insert into ipl_matches  values ('May 27','Final','Final','Mumbai','8:00 PM');
